9bf7da1efd0c4a67539c86a885962e776276a0fe
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2021-11-24 17:24:59 -0500

    updated colors of planner rows to gray when time is in the past

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,25 @@
 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+//Update row coloring if time is in the past
+function timePast(){
+    for(i=0; i<9; i++){
+
+    var plannerRow = $("#row-"+i);
+    var rowHour = 9 + i;
+    var currentHour = moment().format("H");
+    
+    console.log(rowHour);
+    console.log(currentHour);
+
+
+    if(rowHour<currentHour){
+        plannerRow.addClass("table-secondary");
+
+    }
+    }
+}
+
+timePast();
