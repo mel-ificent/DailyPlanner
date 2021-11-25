d55fcb814cabba5bdad876ea600ff645bbcf2cd4
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2021-11-25 10:07:03 -0500

    updated local storage to save planner items and load saved values on the screen

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+//variables
+var btnEl = $(".btn");
+
 //Add today's date to header
 
 var today = moment();
@@ -26,4 +29,35 @@ function timePast(){
     }
 }
 
+//Update page with saved planner items
+function getPlannerItems(){
+    for (i=0;i<9;i++){
+        var item = "text" + i;
+        var itemLS = JSON.parse(localStorage.getItem(item));
+        var plannerRow = $("#row-"+i);
+        if(itemLS !== null){
+            $("#"+item).val(itemLS);
+
+        }
+    }
+
+}
+
+//Save planner Items
+function savePlannerItem(){
+
+    var btnID = $(this).attr('id');
+    var id = btnID.charAt(3);
+    var textID = "text"+id;
+    var plannerText = $("#"+textID).val();
+    localStorage.setItem(textID, JSON.stringify(plannerText));
+
+
+}
+
+
+
+getPlannerItems();
 timePast();
+
+btnEl.on('click',savePlannerItem);
